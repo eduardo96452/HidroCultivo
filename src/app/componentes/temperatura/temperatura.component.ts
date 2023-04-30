60c77c9e6576573b83f51edb976963b32bd168d2
eduardo96452 <107895582+eduardo96452@users.noreply.github.com>
2023-04-30 16:28:31 -0500

    cambios

--- src/app/componentes/temperatura/temperatura.component.ts
+++ src/app/componentes/temperatura/temperatura.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/compat/database';
-import { Observable, map  } from 'rxjs';
-import { ChartConfiguration, ChartDataset, ChartOptions, ChartType, Colors  } from 'chart.js';
-import { BaseChartDirective  } from 'ng2-charts';
+import { Observable, map } from 'rxjs';
+import { ChartConfiguration, ChartDataset, ChartOptions, ChartType, Colors } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
 import { RealTime } from './temperatura.type';
 import { Chart } from 'chart.js/dist';
 //import { Label } from 'chart.js';
@@ -12,44 +12,91 @@ import { Chart } from 'chart.js/dist';
   templateUrl: './temperatura.component.html',
   styleUrls: ['./temperatura.component.css']
 })
-export class TemperaturaComponent{
-  items: any[];
-  temperaturas: number[] = [];
+export class TemperaturaComponent {
+  ultimas10Temperaturas: number[] = [];
   temperatura: number;
+  ultimo10presentacion: number[] = [];
 
-  datos: RealTime = {
-    'CALIDAD DEL AIRE ': 0,
-    "TEMPERATURA ": 0,
-  }
-
-  myChart: Chart;
-
-  title: string = 'El proyecto mas perron';
 
   public barChartLegend = true;
   public barChartPlugins = [];
-   
   public barChartData: ChartConfiguration<'bar'>['data'] = {
-    labels: ['Temperatura'],
     datasets: [
-      { data: [ 0 ]}
+      { data: [0] }
     ]
   }
-
   public barChartOptions: ChartConfiguration<'bar'>['options'] = {
+    responsive: true,
+  }
+
+  public barChartLegend2 = true;
+  public barChartPlugins2 = [];
+  public barChartData2: ChartConfiguration<'bar'>['data'] = {
+    datasets: [
+      { data: [0] }
+    ]
+  }
+  /*public barChartOptions2: ChartConfiguration<'bar'>['options'] = {
+
     responsive: false,
-  }
-  
+  }*/
 
-  constructor(private db:AngularFireDatabase){
-    /*this.db.object('/RealTime').valueChanges().subscribe(data => {
-      console.log(data); // Aquí tienes los datos dentro de la clave "RealTime"
-    });*/
+  public barChartOptions2: ChartOptions = {
+    responsive: true,
+    maintainAspectRatio: false
+  };
 
-  }
+
+
+  constructor(private db: AngularFireDatabase) { }
+
   ngOnInit(): void {
+    let tiempos: string[] = [];
+    const realtimeRef = this.db.list('/HydroGrow/TEMPERATURA ');
 
-    const realtimeRef = this.db.object('/RealTime');
+    realtimeRef.valueChanges().subscribe((temperaturas: number[]) => {
+      this.ultimas10Temperaturas = temperaturas.slice(Math.max(temperaturas.length - 20, 0));
+      this.temperatura = Number(this.ultimas10Temperaturas[this.ultimas10Temperaturas.length - 1].toFixed(2));
+      for (let index = 0; index < this.ultimas10Temperaturas.length; index++) {
+        this.ultimas10Temperaturas[index] = Number(this.ultimas10Temperaturas[index].toFixed(2));
+        tiempos[index] = new Date().toLocaleTimeString();
+      }
+      console.log(this.temperatura);
+      console.log(this.ultimas10Temperaturas);
+      this.ultimo10presentacion[0] = this.temperatura;
+
+      this.barChartData = {
+        labels: [new Date().toLocaleTimeString()],
+        datasets: [
+          {
+
+            data: this.ultimo10presentacion, 
+            label: "Temperatura Actual",
+            backgroundColor: ['rgb(31, 138, 112)'],
+            borderColor: ['rgba(0, 255, 0, 0.2)'],
+            borderWidth: 1
+          },
+        ]
+      }
+
+      this.barChartData2 = {
+        labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20'],
+        datasets: [
+          {
+            data: this.ultimas10Temperaturas, label: "Temperatura Historica",
+            backgroundColor: [
+              'rgba(0, 255, 0, 0.2)'
+            ],
+            borderColor: [
+              'rgb(31, 138, 112)'
+            ],
+            borderWidth: 1
+          }]
+      }
+    });
+
+
+    /*const realtimeRef = this.db.object('/RealTime');
     realtimeRef.valueChanges().subscribe((data: RealTime) => {
       console.log(data);
 
@@ -62,22 +109,7 @@ export class TemperaturaComponent{
         ]
       }
 
-    });
-
-
+    });*/
   }
- 
-
-  lineChartLabels:BaseChartDirective["labels"] = ['00:00'];
-
-  lineChartOptions = { responsive:true, };
-
-  lineChartLegend = true;
-
-  // shufedata(){
-  //   this.lineChartData = [
-  //     { data: this.temperaturas, label:'Temperatura del Agua'}
-  //   ];
-  // }
 
 }
