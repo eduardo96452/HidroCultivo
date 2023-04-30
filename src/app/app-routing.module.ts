dee97a05b56288cd2dc230a075e77992cb7089b2
Karla_Almea <kalmeav@uteq.edu.ec>
2023-04-30 17:57:35 -0500

    calidad del aire

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { LoginComponent } from './componentes/login/login.component';
 import { RegistroComponent } from './componentes/registro/registro.component';
 import { PrincipalComponent } from './componentes/principal/principal.component';
 import { TemperaturaComponent } from './componentes/temperatura/temperatura.component';
+import { AirecalidadComponent } from './componentes/airecalidad/airecalidad.component';
 
 const routes: Routes = [
   {path: '', component:LoginComponent},
   {path: 'registro', component:RegistroComponent},
   {path: 'principal', component:PrincipalComponent},
   {path: 'temperatura', component:TemperaturaComponent},
+  {path: 'airecalidad', component:AirecalidadComponent}
 ];
 
 @NgModule({
