793c524cf0bc342f1fb3c6c911bf1820c799d9a1
eduardo96452 <107895582+eduardo96452@users.noreply.github.com>
2023-04-30 09:30:02 -0500

    cambios masivo

---
+++ src/app/componentes/temperatura/temperatura.type.ts
@@ -0,0 +1,10 @@
+
+
+export interface RealTime {
+    
+    'CALIDAD DEL AIRE '?: number;
+    'LLENADO DE AGUA '?: boolean;
+    'LUMINOSIDAD '?: number;
+    'NIVEL DE PH '?: number;
+    'TEMPERATURA '?: number;
+}
